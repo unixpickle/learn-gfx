944d6dead8957e862349071668969c2683345a9b
Alex Nichol <unixpickle@gmail.com>
2019-01-21 09:55:16 -0500

    drafted triangle raytracing

--- src/raytrace.js
+++ src/raytrace.js
@@ -5,7 +5,7 @@ class Ray {
   }
 }
 
-class Camera {
+class RayCamera {
   constructor(width, height) {
     this.width = width;
     this.height = height;
@@ -17,7 +17,7 @@ class Camera {
   }
 }
 
-class FrustumCamera {
+class FrustumRayCamera extends RayCamera {
   constructor(width, height, fov) {
     super(width, height);
     this.fov = fov;
@@ -39,5 +39,77 @@ class FrustumCamera {
       }
     }
     return result;
+  }
+}
+
+class RayIntersection {
+  constructor(distance, normal) {
+    this.distance = distance;
+    this.normal = normal;
+  }
+}
+
+class RayObject {
+  // Return a RayIntersection or null.
+  intersection(ray) {
+    throw new Error('not implemented');
+  }
+}
+
+class TriangleRayObject extends RayObject {
+  constructor(p1, p2, p3) {
+    super();
+    this.p1 = p1;
+    this.p2 = p2;
+    this.p3 = p3;
+
+    const v1 = this.p2.clone();
+    const v2 = this.p3.clone();
+    v1.sub(this.p1);
+    v2.sub(this.p1);
+
+    this.normal = v1.clone();
+    this.normal.cross(v2);
+    this.normal.normalize();
+
+    const matrix = new THREE.Matrix3();
+    matrix.set(
+      v1.x, v2.x, this.normal.x,
+      v1.y, v2.y, this.normal.y,
+      v1.z, v2.z, this.normal.z,
+    );
+    this.matrix = new THREE.Matrix3();
+    this.matrix.getInverse(matrix);
+  }
+
+  intersection(ray) {
+    const origin = ray.origin.clone();
+    const direction = ray.direction.clone();
+    origin.sub(this.p1);
+
+    origin.applyMatrix3(this.matrix);
+    direction.applyMatrix3(this.matrix);
+    if (Math.abs(direction.z) < 1e-8) {
+      // Parallel to the plane.
+      return null;
+    }
+
+    const t = -origin.z / direction.z;
+    if (t < 0) {
+      // Behind the camera.
+      return null;
+    }
+
+    direction.multiplyScalar(t);
+    origin.add(direction);
+    const a = origin.x;
+    const b = origin.y;
+
+    if (a < 0 || b < 0 || a + b > 1) {
+      // Outside of the triangle.
+      return null;
+    }
+
+    return new RayIntersection(t, this.normal);
   }
 }
